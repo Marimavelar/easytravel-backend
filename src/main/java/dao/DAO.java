c0df67d35f5cbf42d5d54d3130677dfd351468d1
Mariana Marcondes <marimavelar@gmail.com>
2020-05-17 19:58:13 -0300

    Adding UML Diagram

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,12 +1,7 @@
 package dao;
 import java.util.List;
 
-/**
- * Interface DAO
- * 
- * @author Hugo de Paula
- *
- */public interface DAO<T, K> {
+public interface DAO<T, K> {
 	public T get(K chave);
 	public void add(T p);
 	public void update(T p);
